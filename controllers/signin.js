4da314d1b3c45651354c33b4beef5567a49f6dd7
srisubhashp <srisubhashp123@gmail.com>
2021-01-03 23:55:02 -0600

    added files

---
+++ controllers/signin.js
@@ -0,0 +1,30 @@
+const handleSignin= (req, res, db , bcrypt) => {
+
+    const {email, password} = req.body;
+    // we are doign this inorder to remover req.body; before every isntance of the variable.
+
+    if(!email || !password)
+    {
+        return res.status(400).json('incorrect form submission');
+    }
+    db.select('email', 'hash').from('login')
+      .where('email', '=', req.body.email)
+      .then(data => {
+        const isValid = bcrypt.compareSync(password, data[0].hash);
+        if (isValid) {
+          return db.select('*').from('users')
+            .where('email', '=', email)
+            .then(user => {
+              res.json(user[0])
+            })
+            .catch(err => res.status(400).json('unable to get user'))
+        } else {
+          res.status(400).json('wrong credentials')
+        }
+      })
+      .catch(err => res.status(400).json('wrong credentials'))
+  }
+
+  module.exports = {
+      handleSignin: handleSignin
+  }
